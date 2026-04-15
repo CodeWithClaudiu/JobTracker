6cc2a42c89755d50374693ff8f1306438eefe61f
Bryton <brytonkamte@gmail.com>
2026-04-15 16:57:43 +0200

    aggiunto entita jobApplication

---
+++ backend/src/main/java/com/jobtracker/backend/model/JobApplication.java
@@ -0,0 +1,32 @@
+package com.jobtracker.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class JobApplication {
+
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    private String agency;
+
+
+    private String linkJob;
+    private String source;
+
+   @ManyToOne
+    @JoinColumn(name = "placement_profile_id")
+    private PlacementProfile placementProfile; 
+    
+
+}
